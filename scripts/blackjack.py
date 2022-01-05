f39b823dd56ce43e654f7daf35c7800601b1ad8b
KaitoNemoto <s20C1092HN@s.chibakoudai.jp>
2022-01-05 16:55:59 +0000

    Add a new file

---
+++ scripts/blackjack.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+import random 
+
+rospy.init_node('toranpu')
+pub = rospy.Publisher('card', Int32, queue_size=1)  
+rate = rospy.Rate(10)
+n = 0
+
+x = random.randint(1,13)
+y = random.randint(1,13)
+z = random.randint(1,13)
+
+n = x + y + z
+
+print('数は', x, 'と', y, 'と', z)
+print('合わせて', n)
+
+while not rospy.is_shutdown():
+    n += 1
+    pub.publish(n)
+    rate.sleep()
---
+++ scripts/discrimination.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+n = 0
+
+def cb(message):
+    global n
+    n = message.data
+
+    while 1:
+        if n > 21:
+            rospy.loginfo('you lose...')
+        elif n < 21:
+            rospy.loginfo('win!!')
+        else:
+            rospy.loginfo('BlackJack!!!')
+
+if __name__ == '__main__':
+    rospy.init_node('discrimination')
+    sub = rospy.Subscriber('card', Int32, cb) 
+    rospy.spin()
